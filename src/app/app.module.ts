36fe4db2832855444ede8f5c1c879f729bfa1e98
cstonic <scissorfists@gmail.com>
2018-08-07 12:53:18 -0400

    added reorder functionality and toggle switch for it

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,8 @@ import { ListPage } from '../pages/list/list';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { TodoProvider } from '../providers/todo/todo';
+import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -16,6 +18,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     ListPage
   ],
   imports: [
+    HttpClientModule,
     BrowserModule,
     IonicModule.forRoot(MyApp),
   ],
@@ -28,7 +31,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    TodoProvider
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,52 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController, reorderArray } from 'ionic-angular';
+import { TodoProvider } from "../../providers/todo/todo";
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  public todos = [];
+  public reorderIsEnabled = false;
 
-  constructor(public navCtrl: NavController) {
+  constructor(private todoProvider: TodoProvider, public navCtrl: NavController, private alertController: AlertController) {
+    this.todos = this.todoProvider.getTodos();
+  }
 
+  toggleReorder() {
+    this.reorderIsEnabled = !this.reorderIsEnabled;
+  }
+
+  itemReordered($event) {
+    reorderArray(this.todos, $event);
+  }
+
+  openTodoAlert() {
+    let addTodoAlert = this.alertController.create({
+      title: "Add a task",
+      message: "Enter your task",
+      inputs: [
+        {
+          type: "text",
+          name: "addTodoInput"
+        }
+      ],
+      buttons: [
+        {
+          text: "Cancel"
+        },
+        {
+          text: "Add task",
+          handler: (inputData)=> {
+            let todoText;
+            todoText = inputData.addTodoInput;
+            this.todoProvider.addTodo(todoText);
+          }
+        }
+      ]
+    });
+    addTodoAlert.present();
   }
 
 }
